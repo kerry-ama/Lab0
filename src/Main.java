9a8fd8f0f6a4d3850e8c088f76184bcde6f4dcc2
kerry-ama <kaowusu03@gmail.com>
2023-01-12 21:54:02 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
+/**
+ * This is the main class.
+ */
 public class Main {
+    /**
+     * This is the comment for public class.
+     * @param args this is not empty
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
